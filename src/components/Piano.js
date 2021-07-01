6c1ccc450d8df6fb03b0f1f07a7a80c16a2d4584
sulaimanshub <sulaiman-123@hotmail.com>
2021-06-30 23:20:21 -0400

    first

---
+++ src/components/Piano.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Key } from './Key'
+
+class Piano extends React.Component {
+
+    //Key keys = ['do', 'ray', 'mi', 'fa', 'so', 'la', 'ti', 'do']
+
+    render () {
+        return (
+            <div className='key'>
+                <p>keys go here</p>
+            </div>
+        )
+    }
+
+}
+
+export { Piano }
